ea024d45ff2d61e864b6c235560079de5f4c3f51
Mateusz Boczarski <matbocz@outlook.com>
2020-07-23 17:22:04 +0200

    Updates

---
+++ precache-manifest.4dda2bbcf4505b1dc9e1ae7f383b5e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d862962dd521cf81773e8c40005cd22e",
+    "url": "/todo-webapp/index.html"
+  },
+  {
+    "revision": "e7c1b543ffd6ef2463e4",
+    "url": "/todo-webapp/static/css/2.b8f04c25.chunk.css"
+  },
+  {
+    "revision": "e7c1b543ffd6ef2463e4",
+    "url": "/todo-webapp/static/js/2.38da9289.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/todo-webapp/static/js/2.38da9289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603fcc51778afa2a4dae",
+    "url": "/todo-webapp/static/js/main.56ef8c58.chunk.js"
+  },
+  {
+    "revision": "4595a49c32aac4a96e8a",
+    "url": "/todo-webapp/static/js/runtime-main.e62caf07.js"
+  }
+]);
